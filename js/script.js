650b58b2eae144b1e0c757cb435d3cd01873c08a
Amr Gamal <amrgum2033@gmail.com>
2026-02-05 13:22:19 +0200

    feat: update ui dynamically based on user name

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log("Hello, from the console")
+window.addEventListener("load", () => {
+  const overlay = document.querySelector("#overlay");
+  const usernameInput = document.querySelector("#username-input");
+  const submitBtn = document.querySelector("#submit-btn");
+  const greetingText = document.getElementById("name")
+
+  submitBtn.addEventListener("click", () => {
+    const name = usernameInput.value.trim();
+    if (name) {
+      alert(`Welcome, ${name}!`);
+      overlay.style.display = "none";
+      greetingText.innerText = name
+    } else {
+      alert("Please enter your name.");
+    }
+  });
+  usernameInput.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") submitBtn.click();
+  });
+});
