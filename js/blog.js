74ad398bbf6177f8fd078b8ed0de16dcb6115454
Amr Gamal <amrgum2033@gmail.com>
2026-02-06 15:02:39 +0200

    feat: store and render blog posts from local storage

--- js/blog.js
+++ js/blog.js
@@ -1,34 +1,43 @@
 const blogSection = document.querySelector("#blog-posts");
 const themes = ["dark-coffee", "light-coffee"];
 const postForm = document.querySelector("#post-editor form");
-const blogPosts = [
-  {
-    title: "Coffee Break",
-    author: "Grizzle",
-    content: "There is something magical ",
-    publishedAt: "2026-02-05",
-  },
-  {
-    title: "How Coffee Affects Productivity",
-    author: "Jane Doe",
-    content: "I like my coffee so much",
-    publishedAt: "2026-02-05",
-  },
-];
+
+if (!JSON.parse(localStorage.getItem("posts"))) {
+  const blogPosts = [
+    {
+      title: "Coffee Break",
+      author: "Grizzle",
+      content: "There is something magical ",
+      publishedAt: "2026-02-05",
+    },
+    {
+      title: "How Coffee Affects Productivity",
+      author: "Jane Doe",
+      content: "I like my coffee so much",
+      publishedAt: "2026-02-05",
+    },
+  ];
+  localStorage.setItem("posts", JSON.stringify(blogPosts));
+}
 
 function submitPost() {
   const titleInput = document.querySelector("#post-editor form input");
   const contentInput = document.querySelector("#post-editor form textarea");
   const today = new Date().toISOString().split("T")[0];
+  const blogPost = {
+    title: titleInput.value,
+    author: window.name,
+    publishedAt: today,
+    content: contentInput.value,
+  };
 
-  const title = titleInput.value;
-  const content = contentInput.value;
-  const author = window.name
-
-  createNewPost(title, author, today, content);
+  const blogPosts = JSON.parse(localStorage.getItem("posts"))
+  blogPosts.push(blogPost);
+  localStorage.setItem("posts", JSON.stringify(blogPosts));
+  createNewPost(blogPost);
 }
 
-function createNewPost(title, author, publishedAt, content) {
+function createNewPost({ title, author, publishedAt, content }) {
   const newPost = document.createElement("div");
   newPost.className = `blog-post ${themes[themeIndex]}`;
 
@@ -41,11 +50,11 @@ function createNewPost(title, author, publishedAt, content) {
   const postTitle = document.createElement("h2");
   const postAuthor = document.createElement("h6");
   const postDate = document.createElement("h6");
-  const postInfo = document.createElement("div")
+  const postInfo = document.createElement("div");
   const postContent = document.createElement("p");
 
-  postContent.className = "post-content"
-  postInfo.className = "post-info"
+  postContent.className = "post-content";
+  postInfo.className = "post-info";
 
   const titleNode = document.createTextNode(title);
   const authorNode = document.createTextNode(author);
@@ -55,7 +64,7 @@ function createNewPost(title, author, publishedAt, content) {
   postTitle.appendChild(titleNode);
   postAuthor.appendChild(authorNode);
   postDate.appendChild(dateNode);
-  postInfo.append(postAuthor, postDate)
+  postInfo.append(postAuthor, postDate);
   postContent.appendChild(contentNode);
 
   newPost.append(postTitle, postInfo, postContent);
@@ -69,11 +78,6 @@ postForm.addEventListener("submit", (e) => {
 });
 let themeIndex = 0;
 
-for (const blogPost of blogPosts) {
-  createNewPost(
-    blogPost.title,
-    blogPost.author,
-    blogPost.publishedAt,
-    blogPost.content,
-  );
+for (const blogPost of JSON.parse(localStorage.getItem("posts"))) {
+  createNewPost(blogPost);
 }
